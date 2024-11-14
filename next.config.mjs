e6ba56c62bfc6bf68a97e3850b3b8dc8466e58e7
Johns, Erik Michael <erikj3520@gmail.com>
2024-11-13 17:42:12 -0700

    Adjusted Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,5 @@
 /** @type {import('next').NextConfig} */
-import dotenv from 'dotenv';
-dotenv.config();
 
-const nextConfig = {
-    env: {
-        SPOTIFY_CLIENT_ID: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID,
-        SPOTIFY_CLIENT_SECRET: process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_SECRET,
-        SPOTIFY_REDIRECT_URI: process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI,
-        NEXT_PUBLIC_SPOTIFY_SCOPES: process.env.NEXT_PUBLIC_SPOTIFY_SCOPES,
-    },
-    reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
