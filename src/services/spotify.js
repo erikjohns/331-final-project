320b499f21317b8921648162b901ce51e8becaa3
Johns, Erik Michael <erikj3520@gmail.com>
2024-11-13 17:52:01 -0700

    Added Comments

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -1,17 +1,26 @@
+// Import the Spotify Web API library to interact with Spotify's API
 import SpotifyWebApi from 'spotify-web-api-js';
 
 const spotifyApi = new SpotifyWebApi();
 
+/**
+ * Set the access token for the Spotify API Client
+ * This token is required for authenticating API requests
+ * @param {string} token - The access token obtained after user authentication
+ */
 export const setAccessToken = (token) => {
     console.log('Setting access token:', token);
     spotifyApi.setAccessToken(token);
 };
 
-// src/services/spotify.js
+/**
+ * Fetch the current user's top tracks from Spotify
+ * These are the tracks the user has listened to most over a specified time range
+ * @returns {Array} - An array of the user's top track objects, or an empty array if an error occurs
+ */
 export const getTopTracks = async () => {
     try {
         const response = await spotifyApi.getMyTopTracks();
-        console.log("Top Tracks:", response.items); // Add this line
         return response.items;
     } catch (error) {
         console.error("Error fetching top tracks:", error);
@@ -19,10 +28,14 @@ export const getTopTracks = async () => {
     }
 };
 
+/**
+ * Fetch the current user's top artists from Spotify
+ * These are the artists the user listens to most frequently
+ * @returns {Array} - An array of the user's top artist objects, or an empty array if an error occurs
+ */
 export const getTopArtists = async () => {
     try {
         const response = await spotifyApi.getMyTopArtists();
-        console.log("Top Artists:", response.items); // Add this line
         return response.items;
     } catch (error) {
         console.error("Error fetching top artists:", error);
@@ -30,10 +43,13 @@ export const getTopArtists = async () => {
     }
 };
 
+/** Fetch the current user's recently played tracks from Spotify
+ * These are the tracks the user has recently listened to
+ * @returns {Array} - An array f the user's recently played track objects, or an empty array if an error occurs
+ */
 export const getRecentlyPlayed = async () => {
     try {
         const response = await spotifyApi.getMyRecentlyPlayedTracks();
-        console.log("Recently Played:", response.items); // Add this line
         return response.items;
     } catch (error) {
         console.error("Error fetching recently played tracks:", error);
