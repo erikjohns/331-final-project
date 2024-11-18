ce2a7cb94b17754aa23c762a8fa418db6663fc85
Johns, Erik Michael <erikj3520@gmail.com>
2024-11-18 11:20:09 -0700

    Fixed Key Issue

--- src/components/RecentTracksPage.js
+++ src/components/RecentTracksPage.js
@@ -9,7 +9,7 @@ export default function RecentTracksPage({ tracks }) {
             <div className={'track-list-container'}>
                 <ul className={'track-list'}>
                     {tracks.map((track, index) => (
-                        <li key={track.id}>
+                        <li key={index}>
                             <div className={'track'}>
                                 <h1>{index + 1}</h1>
                                 {track.track.album.images.length > 0 && (
