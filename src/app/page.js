e89146383f2f890606be9ac68ba36444c9f5b445
Johns, Erik Michael <erikj3520@gmail.com>
2024-11-18 11:19:58 -0700

    Fixed Styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 import LoginButton from '@/components/LoginButton';
-
+import '@/styles/login.css';
 
 export default function Home() {
     return (
-        <main>
+        <main className={'login-page'}>
             <h1>Welcome to the Spotify Stats App</h1>
             <LoginButton />
         </main>
