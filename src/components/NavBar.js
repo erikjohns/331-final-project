dfb9178479e6d5791814dad27074ed78585b0e22
Johns, Erik Michael <erikj3520@gmail.com>
2024-11-18 13:39:12 -0700

    Made NavBar a Component

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+export default function NavBar() {
+    return (
+        <div className="dashboard-taskbar">
+            <h1 className="taskbar-title">Spotify Stats</h1>
+            <div className="taskbar-buttons">
+                <Link href="/dashboard">
+                    <button>Home Dashboard</button>
+                </Link>
+                <Link href="/top-tracks">
+                    <button>Top Tracks</button>
+                </Link>
+                <Link href="/top-artists">
+                    <button>Top Artists</button>
+                </Link>
+                <Link href="/recent-tracks">
+                    <button>Recent Tracks</button>
+                </Link>
+            </div>
+        </div>
+    )
+}
