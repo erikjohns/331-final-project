40095f06a92715816c6bd9b53e31392d31d2e4e4
Johns, Erik Michael <erikj3520@gmail.com>
2024-11-18 11:19:48 -0700

    Added Spotify Logo

--- src/components/LoginButton.js
+++ src/components/LoginButton.js
@@ -3,7 +3,10 @@ import Link from 'next/link';
 export default function LoginButton() {
     return (
         <Link href="/api/login">
-            <button>Login with Spotify</button>
+            <button>
+                <img src={'/Spotify_Primary_Logo_RGB_White.png'} alt={'Spotify'}/>
+                Login with Spotify
+            </button>
         </Link>
     );
 }
